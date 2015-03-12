534853449de60b6668150ecfce89a205c178bda8
Roman Kostka <roman.kostka@sap.com>
2015-03-12 09:45:44 +0100

    adde snapsvg2

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+
+
 $(document).ready(function(){
 
 
-Snap.load('fox.svg', function (phone) {
-    // Note that we traverse and change attr before SVG is even added to the page (improving performance)
-    phone.selectAll("path[fill='#ff0000']").attr({fill: "#00ff00"});
-    var g = phone.select("g");
-    paper.append(g);    //Now we add the SVG element to the page
-});
+	Snap.load('fox.svg', function (fox) {
+	    // Note that we traverse and change attr before SVG is even added to the page (improving performance)
+	    phone.selectAll("path[fill='#ff0000']").attr({fill: "#00ff00"});
+	    var g = phone.select("g");
+	    paper.append(g);    //Now we add the SVG element to the page
+	});
 
 });
