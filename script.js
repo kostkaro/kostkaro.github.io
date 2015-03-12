25d3e37bc6c1a2628c0346dc03235e793bce1576
Roman Kostka <roman.kostka@sap.com>
2015-03-12 09:50:11 +0100

    adde snapsvg2

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 	var s = Snap(1800, 1600);
 	Snap.load('fox.svg', function (fox) {
 	    // Note that we traverse and change attr before SVG is even added to the page (improving performance)
-	    fox.selectAll("path[fill='#48ADE2']").attr({fill: "#00ff00"});
+	    fox.selectAll("polygon[fill='#48ADE2']").attr({fill: "#00ff00"});
 	    var g = fox.select("g");
 	    s.append(g);    //Now we add the SVG element to the page
 	});
